90a370c2ffe545bfda94f7378cc1e1ee4a3f289c
luccafort <muspellsheim1985@gmail.com>
2017-09-03 12:53:01 +0900

    :+1: first commit
    HTTP/2通信であうか判定するためのコードを実装

---
+++ try_http2.go
@@ -0,0 +1,14 @@
+package main
+import (
+    "fmt"
+    "net/http"
+)
+func main(){
+    resp, err := http.Get("https://google.com")
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    
+    fmt.Printf("Protocol Versino=%s\n", resp.Proto)
+}
